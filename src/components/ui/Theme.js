53d16b4c9db0fee48e54df36bfd55482cc8ad032
Cody <hoban.j.cody@gmail.com>
2021-08-17 17:33:38 -0400

    styling header and adding tabs/buttons

--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -16,10 +16,22 @@ const theme = createTheme({
         secondary: { 
             main: `${arcOrange}`
         },
-        background: {
-            default: "#EAE7D6",
-          }
+    },
+    typography: { 
+        tab: { 
+            fontFamily: "Raleway",
+            textTransform: "none",
+            fontWeight: 700,
+            fontSize: "1rem",
+        },
+        estimate: { 
+            fontFamily: "Pacifico",
+            fontSize: "1rem",
+            textTransform: "none",
+            color: "white"
+        }
     }
+
 });
 
 export default theme;
