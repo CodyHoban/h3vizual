680889b18727c32a53485d08768091f982a67a10
Cody <hoban.j.cody@gmail.com>
2021-08-31 21:40:56 -0400

    lottie overview, and adding animation

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import theme from './ui/Theme'
 import Header from './ui/Header'
 import Footer from './ui/Footer'
+import LandingPage from '../components/LandingPage'
 
 function App() {
   const [selectedIndex, setSelectedIndex] = useState(0);
@@ -22,15 +23,41 @@ function App() {
             setSelectedIndex={setSelectedIndex}
           /> 
           <Switch>
-            <Route exact path="/" component={() => <div style={{height: "2000px"}}>Home</div>} />
-            <Route exact path="/services" component={() => <div>Services</div>} />
-            <Route exact path="/customsoftware" component={() => <div>HCustom Software</div>} />
-            <Route exact path="/mobileapps" component={() => <div>Mobile Apps</div>} />
-            <Route exact path="/websites" component={() => <div>Websites</div>} />
-            <Route exact path="/revolution" component={() => <div>Revolution</div>} />
-            <Route exact path="/about" component={() => <div>About</div>} />
-            <Route exact path="/contact" component={() => <div>Contact</div>} />
-            <Route exact path="/estimate" component={() => <div>Estimate</div>} />
+            <Route 
+              exact 
+              path="/" 
+              component={LandingPage} />
+            <Route 
+              exact 
+              path="/services" component={() => <div>Services</div>} />
+            <Route 
+              exact 
+              path="/customsoftware" 
+              component={() => <div>HCustom Software</div>} />
+            <Route 
+              exact 
+              path="/mobileapps" 
+              component={() => <div>Mobile Apps</div>} />
+            <Route 
+              exact 
+              path="/websites" 
+              component={() => <div>Websites</div>} />
+            <Route 
+              exact 
+              path="/revolution" 
+              component={() => <div>Revolution</div>} />
+            <Route 
+              exact 
+              path="/about" 
+              component={() => <div>About</div>} />
+            <Route 
+              exact 
+              path="/contact" 
+              component={() => <div>Contact</div>} />
+            <Route 
+              exact 
+              path="/estimate" 
+              component={() => <div>Estimate</div>} />
           </Switch>
           <Footer 
             value={value} 
